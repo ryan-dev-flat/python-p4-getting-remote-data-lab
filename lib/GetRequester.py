cf68f5bdf6f75036326775984c976c68e3ba34db
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-07-17 09:41:03 -0600

    Complete references not helpful

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,24 @@
+import http
 import requests
 import json
 
 class GetRequester:
+    url =  f"http://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    response = requests.get(url)
 
     def __init__(self, url):
         self.url = url
 
+
     def get_response_body(self):
-        pass
+        url =  f"http://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = requests.get(self.url)
+
+        return response.content
+
 
     def load_json(self):
-        pass
+        url =  f"http://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = self.get_response_body()
+
+        return json.loads(response.decode())
